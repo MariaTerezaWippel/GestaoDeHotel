b80a0b345985100b207334fe248973caf486da52
Bruna <Carolinereinertbruna@gmail.com>
2023-04-14 08:01:42 -0300

    Alteração código

--- estudodecaso/src/visao/TelaCadastro.java
+++ estudodecaso/src/visao/TelaCadastro.java
@@ -1,6 +1,8 @@
 package visao;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -10,15 +12,13 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
-
-import modelo.Hospede;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
-import java.awt.Font;
-import java.awt.Color;
+
+import modelo.Hospede;
 
 public class TelaCadastro extends JFrame {
 
@@ -65,7 +65,6 @@ public class TelaCadastro extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		
 		JLabel lblNewLabel = new JLabel("Nome:");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel.setBounds(120, 124, 46, 14);
@@ -77,7 +76,6 @@ public class TelaCadastro extends JFrame {
 		contentPane.add(txtNome);
 		txtNome.setColumns(10);
 
-		
 		JLabel lblNewLabel_1 = new JLabel("CPF:");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel_1.setBounds(131, 168, 35, 14);
@@ -88,7 +86,6 @@ public class TelaCadastro extends JFrame {
 		contentPane.add(txtCpf);
 		txtCpf.setColumns(10);
 
-		
 		JLabel lblNewLabel_2 = new JLabel("Email:");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel_2.setBounds(131, 214, 46, 14);
@@ -100,7 +97,6 @@ public class TelaCadastro extends JFrame {
 		contentPane.add(txtEmail);
 		txtEmail.setColumns(10);
 
-		
 		JLabel lblNewLabel_3 = new JLabel("Telefone:");
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel_3.setBounds(118, 257, 87, 14);
@@ -112,7 +108,6 @@ public class TelaCadastro extends JFrame {
 		contentPane.add(txtTelefone);
 		txtTelefone.setColumns(10);
 
-		
 		JLabel lblNewLabel_4 = new JLabel("Data de Nascimento:");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel_4.setBounds(45, 301, 128, 14);
@@ -123,7 +118,6 @@ public class TelaCadastro extends JFrame {
 		contentPane.add(txtDataNascimento);
 		txtDataNascimento.setColumns(10);
 
-		
 		JLabel lblNewLabel_5 = new JLabel("Quantidade de Hóspedes:");
 		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel_5.setBounds(20, 349, 167, 14);
@@ -134,7 +128,6 @@ public class TelaCadastro extends JFrame {
 		txtQuanHospede.setBounds(207, 348, 46, 20);
 		contentPane.add(txtQuanHospede);
 		txtQuanHospede.setColumns(10);
-
 
 		JLabel lblNewLabel_6 = new JLabel("CEP:");
 		lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 14));
@@ -163,26 +156,26 @@ public class TelaCadastro extends JFrame {
 				Hospede hospedagem = new Hospede();
 
 				ArrayList<Hospede> hospedagemLista = new ArrayList();
-				
-				if(nome.isEmpty()) {
+
+				if (nome.isEmpty()) {
 					JOptionPane.showMessageDialog(null, "Nome errado");
-				}else{
+				} else {
 					hospedagem.setNome(nome);
 				}
-				if(cpf.isEmpty()) {
+				if (cpf.isEmpty()) {
 					JOptionPane.showMessageDialog(null, "cpf errado");
-				}else{
+				} else {
 					Long cpfLong = Long.valueOf(cpf);
 					hospedagem.setCpf(cpfLong);
 				}
-				if(email.isEmpty()) {
+				if (email.isEmpty()) {
 					JOptionPane.showMessageDialog(null, "email errado");
-				}else{
+				} else {
 					hospedagem.setEmail(email);
 				}
-			
+
 				hospedagem.setDataNascimento(dataNascimento);
-				
+
 				Integer numeroP = Integer.valueOf(quantidadeDeHospedes);
 				hospedagem.setNumeroPessoas(numeroP);
 				Long cepLong = Long.valueOf(cep);
@@ -211,8 +204,6 @@ public class TelaCadastro extends JFrame {
 		table.setModel(dataModel);
 		scrollPane.setViewportView(table);
 
-
-		
 		JLabel lblNewLabel_7 = new JLabel(" Cadastro Hóspede");
 		lblNewLabel_7.setFont(new Font("Tahoma", Font.PLAIN, 23));
 		lblNewLabel_7.setBounds(383, 31, 304, 23);
