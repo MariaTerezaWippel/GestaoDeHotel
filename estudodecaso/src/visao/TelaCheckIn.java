9aad90c2fc81d39a2bd351ff464293ad8187c7b9
BrunaReinert <carolinereinertbruna@gmail.com>
2023-06-06 14:49:38 -0300

    Atualizado

--- estudodecaso/src/visao/TelaCheckIn.java
+++ estudodecaso/src/visao/TelaCheckIn.java
@@ -19,7 +19,6 @@ public class TelaCheckIn extends JFrame {
 
 	private JPanel contentPaneTela;
 	private JTextField textNumeroReserva;
-	private JTextField textField_Hospede;
 
 	/**
 	 * Launch the application.
@@ -29,7 +28,6 @@ public class TelaCheckIn extends JFrame {
 			public void run() {
 				try {
 					TelaCheckIn frame = new TelaCheckIn();
-					frame.setExtendedState(MAXIMIZED_BOTH);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -44,7 +42,7 @@ public class TelaCheckIn extends JFrame {
 	public TelaCheckIn() {
 		setTitle("Tela Efetuar Check-In");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1448, 990);
+		setBounds(100, 100, 455, 237);
 		contentPaneTela = new JPanel();
 		contentPaneTela.setForeground(new Color(0, 0, 0));
 		contentPaneTela.setBackground(new Color(135, 206, 250));
@@ -54,31 +52,26 @@ public class TelaCheckIn extends JFrame {
 		contentPaneTela.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Efetuar Check-In");
-		lblNewLabel.setBounds(640, 178, 335, 48);
+		lblNewLabel.setBounds(70, 11, 335, 48);
 		lblNewLabel.setForeground(new Color(25, 25, 112));
 		lblNewLabel.setBackground(new Color(240, 240, 240));
-		lblNewLabel.setFont(new Font("Dialog", Font.BOLD | Font.ITALIC, 38));
+		lblNewLabel.setFont(new Font("Source Serif Pro Semibold", Font.BOLD | Font.ITALIC, 34));
 		contentPaneTela.add(lblNewLabel);
 		
 		textNumeroReserva = new JTextField();
-		textNumeroReserva.setBounds(822, 418, 153, 23);
+		textNumeroReserva.setBounds(238, 89, 153, 23);
 		textNumeroReserva.setForeground(new Color(0, 0, 0));
 		contentPaneTela.add(textNumeroReserva);
 		textNumeroReserva.setColumns(10);
 
 		
 		JLabel lblNewLabel_1 = new JLabel("Número da Reserva : ");
-		lblNewLabel_1.setBounds(597, 419, 215, 22);
+		lblNewLabel_1.setBounds(28, 86, 215, 22);
 		lblNewLabel_1.setFont(new Font("Source Serif Pro Semibold", Font.BOLD | Font.ITALIC, 20));
 		contentPaneTela.add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("Nome Hóspede(s) : ");
-		lblNewLabel_2.setBounds(610, 485, 191, 20);
-		lblNewLabel_2.setFont(new Font("Source Serif Pro Semibold", Font.BOLD | Font.ITALIC, 20));
-		contentPaneTela.add(lblNewLabel_2);
-		
 		JButton btnConfirmar = new JButton("Confimar Check-In");
-		btnConfirmar.setBounds(696, 583, 203, 33);
+		btnConfirmar.setBounds(26, 141, 203, 33);
 		btnConfirmar.setForeground(new Color(255, 255, 255));
 		btnConfirmar.addActionListener(new ActionListener() {
 
@@ -89,28 +82,9 @@ public class TelaCheckIn extends JFrame {
 		btnConfirmar.setFont(new Font("Source Serif Pro Semibold", Font.BOLD | Font.ITALIC, 18));
 		contentPaneTela.add(btnConfirmar);
 		
-		textField_Hospede = new JTextField();
-		textField_Hospede.setBounds(822, 487, 153, 23);
-		textField_Hospede.setForeground(Color.BLACK);
-		textField_Hospede.setColumns(10);
-		contentPaneTela.add(textField_Hospede);
-		
-		
-		
-		JLabel lblNewLabel_3 = new JLabel("New label");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		lblNewLabel_3.setBounds(542, 237, 433, 100);
-		lblNewLabel_3.setIcon(new ImageIcon(TelaCheckIn.class.getResource("/Imagens/LogoHotel.png")));
-		contentPaneTela.add(lblNewLabel_3);
-		
-		JLabel lblNewLabel_6 = new JLabel("New label");
-		lblNewLabel_6.setBounds(-622, -95, 1293, 520);
-		lblNewLabel_6.setIcon(new ImageIcon(TelaCheckIn.class.getResource("/Imagens/icon.png")));
-		contentPaneTela.add(lblNewLabel_6);
-		
-		JLabel lblNewLabel_7 = new JLabel("New label");
-		lblNewLabel_7.setBounds(-622, 408, 1232, 425);
-		lblNewLabel_7.setIcon(new ImageIcon(TelaCheckIn.class.getResource("/Imagens/Design sem nome (1).png")));
-		contentPaneTela.add(lblNewLabel_7);
+		JButton btnNewButton = new JButton("Voltar");
+		btnNewButton.setFont(new Font("Source Serif Pro Semibold", Font.BOLD | Font.ITALIC, 15));
+		btnNewButton.setBounds(263, 141, 107, 35);
+		contentPaneTela.add(btnNewButton);
 	}
 }
